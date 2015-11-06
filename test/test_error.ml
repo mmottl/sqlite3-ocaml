913ac0cff1113c9e691018bc066be35b9925d9a7
Markus Mottl <markus.mottl@gmail.com>
2015-11-06 14:58:44 -0500

    Improved test application for failing functions

--- test/test_error.ml
+++ test/test_error.ml
@@ -1,8 +1,12 @@
 open Sqlite3
 
-(* Tests our ability to return an error from a user defined function *)
+exception This_function_always_fails
+
+(* Tests our ability to raise an exception from a user-defined function *)
 let () =
   let db = db_open "t" in
-  create_fun0 db "MYERROR" (fun () -> failwith "This function always errors");
-  let res = exec db "SELECT MYERROR();" in
-  prerr_endline ("Should have thrown an error: " ^ Rc.to_string res)
+  create_fun0 db "MYERROR" (fun () -> raise This_function_always_fails);
+  try
+    let res = exec db "SELECT MYERROR();" in
+    prerr_endline ("Should have thrown an error: " ^ Rc.to_string res)
+  with This_function_always_fails -> print_endline "Ok"
