9a29f1a9da9095128f82270023f3438dd219c8ba
Leonid Rozenberg <leonidr@gmail.com>
2016-10-04 12:05:45 -0400

    Build and test on travis

---
+++ travis_tests.sh
@@ -0,0 +1,26 @@
+build_on_linux () {
+  ocaml setup.ml -build
+}
+
+build_on_osx () {
+  env SQLITE3_OCAML_BREWCHECK=1 ocaml setup.ml -build
+}
+
+echo building
+ocaml setup.ml -configure -prefix prefix
+
+case $TRAVIS_OS_NAME in
+  osx) build_on_osx ;;
+  linux) build_on_linux ;;
+  *) echo "Unknown $TRAVIS_OS_NAME"; exit 1
+esac
+
+echo building tests
+ocaml setup.ml -configure --enable-tests
+case $TRAVIS_OS_NAME in
+  osx) build_on_osx ;;
+  linux) build_on_linux ;;
+  *) echo "Unknown $TRAVIS_OS_NAME"; exit 1
+esac
+
+ocaml setup.ml -test
