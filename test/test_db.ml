471c495c94bb76ea2da4d07361b01bdd16733ff6
Ted Spence <tspence@fb.com>
2019-03-13 16:15:07 -0700

    Removed a bit of unnecessary logging; added in db close tests

--- test/test_db.ml
+++ test/test_db.ml
@@ -1,11 +1,12 @@
 open Sqlite3
 
 let%test "test_db" =
-  for i = 1 to 1000 do
-    Printf.printf "->%d\n%!" i;
-    ignore (db_open "t");
-    Printf.printf "<-%d\n%!" i;
+  for _ = 1 to 1000 do
+    let db = db_open "t" in
     Gc.full_major ();
+    let _ = db_close db in
+    ()
   done;
+  print_endline "Able to open and close database";
   true
   ;;
