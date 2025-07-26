3cbf5af72f5eaf0676e3aa002eaec754d2ef6a7a
Braulio Valdivielso <brlvldvlsmrtnz@gmail.com>
2025-07-26 14:49:18 +0200

    keep adding tests

--- test/test_carrays.ml
+++ test/test_carrays.ml
@@ -56,23 +56,100 @@ let%test "carray_lifetime_is_bound_to_stmt" =
   assert (0 = List.compare compare_query_row (get_all stmt) expected);
   true
 
-let make_carray size =
-  let ba = Bigarray.(Array1.create int64 c_layout size) in
-  CArray.of_int64_bigarray ba
+let make_bigarray size = Bigarray.(Array1.create int64 c_layout size)
+
+let evaluate_if_collected obj =
+  let was_collected = ref false in
+  Gc.finalise_last (fun _ -> was_collected := true) obj;
+  was_collected
 
 let%test "memory_for_bigarray_is_released_when_finalizing_statement" =
   let db = db_open ":memory:" in
   let was_array_collected, stmt =
-    let ba = Bigarray.(Array1.create int64 c_layout 10) in
-    let was_array_collected = ref false in
-    Gc.finalise_last (fun () -> was_array_collected := true) ba;
+    let ba = make_bigarray 10 in
+    let was_array_collected = evaluate_if_collected ba in
     let stmt = prepare db "SELECT value FROM carray($ptr)" in
     assert (Rc.OK = bind_carray stmt 1 (CArray.of_int64_bigarray ba));
     (was_array_collected, stmt)
   in
+
+  (* at first, the bigarray is not collected because it's bound to the
+     statement *)
   Gc.full_major ();
   assert (not !was_array_collected);
+
+  (* but after finalizing the statement *)
   Rc.check (finalize stmt);
+
+  (* the array can get collected *)
   Gc.full_major ();
   assert !was_array_collected;
+  true
+
+let%test "rebinding carray makes previously-bound carray collectable" =
+  let db = db_open ":memory:" in
+  let was_array_collected, stmt =
+    let ba = make_bigarray 10 in
+    let was_array_collected = evaluate_if_collected ba in
+    let stmt = prepare db "SELECT value FROM carray($ptr)" in
+    assert (Rc.OK = bind_carray stmt 1 (CArray.of_int64_bigarray ba));
+    (was_array_collected, stmt)
+  in
+  (* at first, the bigarray is not collected because it's bound to the
+     statement *)
+  Gc.full_major ();
+  assert (not !was_array_collected);
+
+  (* but then we bind a different carray to the same position in the
+     statement *)
+  let new_carray = make_bigarray 10 |> CArray.of_int64_bigarray in
+  assert (Rc.OK = bind_carray stmt 1 new_carray);
+
+  (* and then the array does get collected *)
+  Gc.full_major ();
+  assert !was_array_collected;
+  true
+
+let make_sql_query_with_n_carrays n =
+  let carray_part = List.init n (fun i -> "carray(?) as c" ^ string_of_int i) in
+  "SELECT * FROM " ^ String.concat ", " carray_part
+
+let%test "rebinding_clears_only_one_of_the_bigarrays" =
+  let n = 3 in
+  let db = db_open ":memory:" in
+  let collections, stmt =
+    let arrays = List.init n (fun _ -> make_bigarray 10) in
+    let collections = List.map evaluate_if_collected arrays in
+    let stmt = prepare db (make_sql_query_with_n_carrays n) in
+    List.iteri
+      (fun i array ->
+        assert (
+          Rc.OK = bind_carray stmt (i + 1) (CArray.of_int64_bigarray array)))
+      arrays;
+    (collections, stmt)
+  in
+
+  (* at first, no carray gets collected (they are all bound to the statement) *)
+  Gc.full_major ();
+  assert (List.for_all (fun was_collected -> not !was_collected) collections);
+
+  (* when we rebind parameter 1 *)
+  assert (
+    Rc.OK = bind_carray stmt 1 (CArray.of_int64_bigarray (make_bigarray 10)));
+
+  (* only that one gets collected *)
+  Gc.full_major ();
+  assert !(List.nth collections 0);
+  assert (not !(List.nth collections 1));
+  assert (not !(List.nth collections 2));
+
+  (* when we rebind parameter 3 *)
+  assert (
+    Rc.OK = bind_carray stmt 3 (CArray.of_int64_bigarray (make_bigarray 10)));
+
+  (* that one gets collected too *)
+  Gc.full_major ();
+  assert !(List.nth collections 2);
+  assert (not !(List.nth collections 1));
+
   true
